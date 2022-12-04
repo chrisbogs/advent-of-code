85dcb46a50fde28ef03de36c6aa24d3017b91121
chris <chris.bognar@cmsmontera.com>
2022-12-04 11:12:53 -0500

    Day 4 completed

--- tests/Test2022.cs
+++ tests/Test2022.cs
@@ -62,5 +62,35 @@ namespace tests
         {
             Assert.Equal(expected, TwentyTwentyTwo.Day3Part2(input));
         }
+
+        [Theory]
+        [InlineData(new string[] {
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8",
+            }, 2)]
+        public void Day4Part1(string[] input, int expected)
+        {
+            Assert.Equal(expected, TwentyTwentyTwo.Day4Part1(input));
+        }
+        [Theory]
+        [InlineData(new string[] {
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8",
+            }, 4)]
+        public void Day4Part2(string[] input, int expected)
+        {
+            Assert.Equal(expected, TwentyTwentyTwo.Day4Part2(input));
+        }
+
+
+
     }
 }
