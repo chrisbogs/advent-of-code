fca272eacc8b74973d444a98ea7fc44e71fd21fe
chris <chris.bognar@cmsmontera.com>
2023-10-26 12:00:37 -0400

    Added stub for 2023

---
+++ shared/Logic/TwentyTwentyThree.cs
@@ -0,0 +1,231 @@
+﻿namespace AdventOfCodeShared.Logic
+{
+    public class TwentyTwentyThree
+    {
+        public static long Day1Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day1Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day2Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day2Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day3Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day3Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day4Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day4Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day5Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day5Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day6Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day6Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day7Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day7Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day8Part1(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day8Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day9Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day9Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static long Day10Part1(string[] input)
+        {
+            return 0;
+        }
+        public static long Day10Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day11Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day11Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day12Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day12Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day13Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day13Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day14Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day14Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day15Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day15Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day16Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day16Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day17Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day17Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day18Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day18Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day19Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day19Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day20Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day20Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day21Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day21Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day22Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day22Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day23Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day23Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day24Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day24Part2(string[] input)
+        {
+            return 0;
+        }
+
+        public static int Day25Part1(string[] input)
+        {
+            return 0;
+        }
+        public static int Day25Part2(string[] input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ tests/Test2023.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCodeShared.Logic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace tests
+{
+    public class Test2023
+    {
+        //[Theory]
+        //public void Day1(string[] input, int expected)
+        //{
+        //    Assert.Equal(expected, TwentyTwentyThree.Day1Part1(input));
+        //}
+
+        //[Theory]
+        //[InlineData(new string[] { "1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000" }, 45000)]
+        //public void Day1Part2(string[] input, int expected)
+        //{
+        //    Assert.Equal(expected, TwentyTwentyThree.Day1Part2(input));
+        }
+
+       
+}
