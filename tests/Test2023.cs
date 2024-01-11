98ecfaaeb713f2b3f14e42e726af516e006d9c13
chris <chris.bognar@cmsmontera.com>
2024-01-11 17:27:26 -0500

    Finished day 4

--- tests/Test2023.cs
+++ tests/Test2023.cs
@@ -34,7 +34,7 @@ namespace Tests
         [InlineData(new string[] { "aone2" }, 12)]
         [InlineData(new string[] { "aonetwo" }, 12)]
         [InlineData(new string[] { "aonetwoerdfvsrgy" }, 12)]
-        public void Day1Part2(string[] input, int expected)
+        public void TestDay1Part2(string[] input, int expected)
         {
             Assert.Equal(expected, TwentyTwentyThree.Day1Part2(input));
         }
@@ -78,7 +78,7 @@ namespace Tests
         , "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
         , "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
         , "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green" }, 2286)]
-        public void Day2Part2(string[] input, int expected)
+        public void TestDay2Part2(string[] input, int expected)
         {
             Assert.Equal(expected, TwentyTwentyThree.Day2Part2(input));
         }
@@ -112,9 +112,35 @@ namespace Tests
             "......755.",
             "...$.*....",
             ".664.598.." }, 467835)]
-        public void Day3Part2(string[] input, int expected)
+        public void TestDay3Part2(string[] input, int expected)
         {
             Assert.Equal(expected, TwentyTwentyThree.Day3Part2(input));
+        }
+
+        [Theory]
+        [InlineData(new string[] {
+             "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+             "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+             "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+             "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+             "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+             "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"}, 13)]
+        public void TestDay4(string[] input, int expected)
+        {
+            Assert.Equal(expected, TwentyTwentyThree.Day4Part1(input));
+        }
+
+        [Theory]
+        [InlineData(new string[] {
+             "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+             "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+             "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+             "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+             "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+             "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"}, 30)]
+        public void TestDay4Part2(string[] input, int expected)
+        {
+            Assert.Equal(expected, TwentyTwentyThree.Day4Part2(input));
         }
     }  
 }
