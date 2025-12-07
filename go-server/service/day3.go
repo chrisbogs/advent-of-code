e409e6f1f6dd2162e9e3af1f8f1e999f9913d5ad
Kayla Bognar <kaylabognar@Kaylas-MacBook-Air.local>
2025-12-06 23:48:48 -0500

    WIP part 2

--- go-server/service/day3.go
+++ go-server/service/day3.go
@@ -5,7 +5,6 @@ import (
 )
 
 func Day3Part1(input []string) int {
-
 	sum := 0
 	for _, line := range input {
 		// find max 2 digits with their indices
@@ -31,4 +30,47 @@ func Day3Part1(input []string) int {
 		sum += num
 	}
 	return sum
+}
+
+func Day3Part2(input []string) int {
+	sum := 0
+	combined := ""
+	for _, line := range input {
+		combined += line
+	}
+	parsedInts := make([]int, len(combined))
+	for i, char := range combined {
+		parsedInts[i], _ = strconv.Atoi(string(char))
+	}
+
+	digits := make([]int, 0)
+	newIndex := 0
+	maxVal := -1
+	for i := range 12 {
+		maxVal, newIndex = FindMax(newIndex, 12-i, parsedInts)
+		println(newIndex, 12-i, maxVal, newIndex)
+		digits = append(digits, maxVal)
+	}
+
+	numStr := ""
+	for _, digit := range digits {
+		numStr += strconv.Itoa(digit)
+	}
+	num, _ := strconv.Atoi(numStr)
+	sum += num
+
+	return sum
+}
+
+func FindMax(start int, n int, arr []int) (int, int) {
+	maxValue := -1
+	index := -1
+	for i := start; i < len(arr)-n; i++ {
+		val := arr[i]
+		if val > maxValue {
+			maxValue = val
+			index = i
+		}
+	}
+	return maxValue, index + 1
 }
--- go-server/service/day3_test.go
+++ go-server/service/day3_test.go
@@ -21,3 +21,31 @@ func TestDay3Part1A(t *testing.T) { assert.Equal(t, 98, Day3Part1([]string{"9876
 func TestDay3Part1B(t *testing.T) { assert.Equal(t, 89, Day3Part1([]string{"811111111111119"})) }
 func TestDay3Part1C(t *testing.T) { assert.Equal(t, 78, Day3Part1([]string{"234234234234278"})) }
 func TestDay3Part1D(t *testing.T) { assert.Equal(t, 92, Day3Part1([]string{"818181911112111"})) }
+
+func TestDay3Part2(t *testing.T) {
+	expected := 357
+	input := `987654321111111
+811111111111119
+234234234234278
+818181911112111`
+	actual := Day3Part2(strings.Split(input, "\n"))
+
+	assert.Equal(t, expected, actual)
+}
+func TestDay3Part2A(t *testing.T) {
+	maxVal, index := FindMax(0, 0, []int{8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1})
+	assert.Equal(t, 9, maxVal)
+	assert.Equal(t, 6, index)
+}
+func TestDay3Part2A1(t *testing.T) {
+	assert.Equal(t, 987654321111, Day3Part2([]string{"987654321111111"}))
+}
+func TestDay3Part2B(t *testing.T) {
+	assert.Equal(t, 811111111119, Day3Part2([]string{"811111111111119"}))
+}
+func TestDay3Part2C(t *testing.T) {
+	assert.Equal(t, 434234234278, Day3Part2([]string{"234234234234278"}))
+}
+func TestDay3Part2D(t *testing.T) {
+	assert.Equal(t, 888911112111, Day3Part2([]string{"818181911112111"}))
+}
